564e622ebe96ee322eb4ea2dd8656ee8ad1d2751
Meeeelira <lirahasibuan21@gmail.com>
2024-05-19 21:37:04 +0700

    Create DB.php

---
+++ DB.php
@@ -0,0 +1,5 @@
+<?php
+$koneksi=mysqli_connect('localhost','root','','db_perpustakaan4c')or die('Database tidak ditemukan');
+//jalankan uri: localhost/si4c/DB.php
+//jika tampilan kosong,artinya koneksi berhasil
+?>
